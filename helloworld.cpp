933204b808730144334ebab5681f541dcf09fbd8
Vuong Yen <vuonghaiyen.94@gmail.com>
2021-01-29 07:40:09 +0700

    1st commit

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using  namespace std;
+int main()
+{	
+    string name;
+    cout << "What's your name?" << endl;
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+    return 0;
+}
